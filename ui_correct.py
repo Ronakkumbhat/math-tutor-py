ea8112c662557f34afc8a8471af9c1bbc31a7803
Ronak Kumbhat <ronakkumbhat8@gmail.com>
2024-03-18 21:54:57 +0530

    correct screen

---
+++ ui_correct.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'correct.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QSizePolicy, QWidget)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(800, 600)
+        self.label = QLabel(Form)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(300, 150, 261, 81))
+        font = QFont()
+        font.setPointSize(20)
+        self.label.setFont(font)
+        self.label_2 = QLabel(Form)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(240, 230, 351, 291))
+        self.label_2.setPixmap(QPixmap(u"images/excellent-1.gif"))
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.label.setText(QCoreApplication.translate("Form", u"Awesome!", None))
+        self.label_2.setText("")
+    # retranslateUi
+
